f594c10bb560818f4bdb6668696db0d6f7305b7e
andrew <azarkon@gmail.com>
2016-01-20 17:04:06 -0500

    refactor and clean code

--- anagrammer.rb
+++ anagrammer.rb
@@ -64,8 +64,8 @@ class Alphagram
     remainder_gram = self.to_s
     self_i = 0
     other_i = 0
-
     while other_i < other.size 
+
       case remainder_gram[self_i] <=> other.gram[other_i]
       when 0
         remainder_gram[self_i] = ""
@@ -75,7 +75,9 @@ class Alphagram
       when 1, nil
         return nil # other contains a character not in self and cannot be subtracted from self
       end
+
     end
+
     return Alphagram.new(remainder_gram, false)
   end
 
@@ -96,11 +98,7 @@ class Alphagram
     subs.keys.each do |key1|
       keys[(keys.index(key1) + 1..-1)].each do |key2|
         next unless self === subs[key1] + subs[key2] 
-        subs[key1].phrases.each do |phrase1|
-          subs[key2].phrases.each do |phrase2|
-            out << phrase1.to_s + " " + phrase2.to_s
-          end
-        end
+        out.merge form_phrases(key1, key2)
       end
     end
     out
@@ -109,6 +107,19 @@ class Alphagram
   # Allows hash to reflect changes in nested sub-alphagram structure
   def hash
     gram.hash ^ words.hash ^ (subs.values.inject(0) { |hash, gram| hash ^ gram.hash })
+  end
+
+  private
+
+  def form_phrases key1, key2
+    out = PhraseSet.new
+    subs[key1].phrases.each do |phrase1|
+      subs[key2].phrases.each do |phrase2|
+        out << phrase1.to_s + " " + phrase2.to_s
+      end
+    end
+
+    out
   end
 
 end
@@ -138,7 +149,7 @@ class AlphagramCollection < Hash
 
   # Allows hash value to reflect nested changes
   def hash
-    super ^ (self.values.inject { |hash, gram| hash ^ gram.hash })
+    super ^ (self.values.inject(0) { |hash, gram| hash ^ gram.hash })
   end
 
 end
@@ -147,24 +158,25 @@ class AnagramFinder
 
   # Does not presume that an anagram will be the same number of words as the input phrase.
   # Optimizes typical but not worst-case runtime by prioritizing validation of candidate phrases with longer and fewer words.
-  attr_accessor :target_alphagram, :sub_grams, :md5, :candidates, :checked_candidates
+  attr_accessor :sub_grams, :md5, :candidates, :checked_candidates
+  attr_reader :phrase
 
-  def initialize(phrase, hash, word_file = "wordlist.txt")
-    @file = word_file
+  def initialize(phrase)
     @phrase = phrase
-    @target_alphagram = Alphagram.new phrase
-    @md5 = hash
     @sub_grams = AlphagramCollection.new
-    @candidates = Array.new
-    @checked_candidates = Set.new
   end
 
-  def self.find(*args)
-    finder = AnagramFinder.new(*args)
-    finder.parse_and_validate
+  def target_alphagram
+    @alphagram ||= Alphagram.new phrase
   end
 
-  def parse_and_validate
+  def self.find(phrase, md5)
+    finder = AnagramFinder.new(phrase)
+    finder.parse_and_validate (md5)
+  end
+
+  def parse_and_validate(md5)
+    @md5 = md5
     # parses wordlist file and gropus words by length
     buckets = bucketize_words
 
@@ -173,8 +185,10 @@ class AnagramFinder
       buckets[key].each do |word|
         parse_word word
       end
+
       result = find
       return result if result
+
     end
     nil # no valid candidates found
   end
@@ -188,63 +202,61 @@ class AnagramFinder
     existing = sub_grams[new_gram.to_s]
     if existing
       existing.add_word word
-      return nil
+
+            # calculate the complementary alphagram for word
+    elsif complementray_gram = target_alphagram - new_gram
+
+      if known_complementary_gram = sub_grams[complementray_gram.to_s]
+        candidates << new_gram + known_complementary_gram
+      else
+        #add new_gram to all sub_grams and adds valid combinations to sub_grams 
+        combine new_gram, complementray_gram 
+
+        sub_grams.push new_gram
+      end
     end
-
-    # calculate the complementary alphagram for word
-    remainder = target_alphagram - new_gram
-    return nil if remainder.nil? # word cannot be part of the anagram phrase
-
-    # find if complementary alphagram exists in sub_grams
-    remainder_existing = sub_grams[remainder.to_s]
-    if remainder_existing
-      candidates << new_gram + remainder_existing
-      return nil
-    end
-
-   #add new_gram to all sub_grams and adds valid combinations to sub_grams 
-    combine new_gram, remainder  
-
-    sub_grams.push new_gram
 
     nil
   end
 
-   # 
   def find
+    @candidates ||= Array.new
     candidates.each do |candidate|
       # Skips candidates that have already been checked
-      next if checked_candidates.include? candidate.hash
-      checked_candidates << candidate.hash
+      next if logged_candidate? candidate
 
       candidate.phrases.each do |phrase|
-
         # Caluclates all permutations of phrase and checks them against the md5 hash
         validated_phrase = validate phrase
         return validated_phrase if validated_phrase
+
       end
     end
     nil #no valid candidates found
   end
 
 
-  def bucketize_words
-    f = File.open @file
+  def bucketize_words (file = "wordlist.txt")
+    f = File.open file
     word_buckets = Hash.new
 
     while word = f.gets
-      word.chomp!
-      # cleaning input a little
-      next unless word.index /[aeiouy]/
-      word.slice!(-2, 2) if word[-2] == "\'"
+      clean_word (word)
 
       word_buckets[word.length] ||= Array.new
       word_buckets[word.length].push word
-
     end
 
     f.close
     word_buckets
+  end
+
+  def clean_word word
+    word.chomp!
+    word.slice!(-2, 2) if word[-2] == "\'"
+    return nil if word.strip == "" ||  (word.index /[aeiouy]/).nil?
+
+    word
   end
 
   def validate phrase
@@ -256,11 +268,11 @@ class AnagramFinder
     nil
   end
 
-  def combine gram, remainder
+  def combine gram, complementray_gram
     new_subs = AlphagramCollection.new
     sub_grams.each_value do |sub|
 
-      next if (remainder - sub).nil?
+      next if (complementray_gram- sub).nil?
 
       new_subs.push sub + gram
     end
@@ -268,6 +280,15 @@ class AnagramFinder
     sub_grams.merge! new_subs
   end
 
+  def logged_candidate? candidate
+      @checked_candidates ||= Set.new
+      return true if checked_candidates.include? candidate.hash
+
+      # logs candidate if not yet logged
+      checked_candidates << candidate.hash
+
+      false
+    end
 end
 
 
